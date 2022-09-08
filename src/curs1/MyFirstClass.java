14272ada652bd10ef4ae5f81c5683a633773027d
Dragos Tanta <dragostanta@Dragoss-MacBook-Pro.local>
2022-09-08 21:39:21 +0300

    curs1

---
+++ src/curs1/MyFirstClass.java
@@ -0,0 +1,17 @@
+package curs1;
+
+public class MyFirstClass {
+	
+
+	String nume = "Dragos ";//variabila de instanta
+	int age = 23;
+
+	public static void main(String[] args) {
+
+		MyFirstClass obj = new MyFirstClass();
+		
+		System.out.println("Salut " + obj.nume + obj.age);
+		
+	}
+
+}
