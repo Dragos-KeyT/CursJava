656e0276d2ec916308a9d80c5cfd8c3f2e18a050
Dragos Tanta <dragos.tanta@gmail.com>
2022-10-10 21:23:43 +0300

    curs10

---
+++ src/curs10/Customer.java
@@ -0,0 +1,17 @@
+package curs10;
+
+public class Customer {
+	
+	public String name;
+	public String adress;
+	public String email;
+	
+	public Customer(String name, String adress, String email) {
+		this.name = name;
+		this.adress = adress;
+		this.email = email;
+	}
+	
+	
+
+}
