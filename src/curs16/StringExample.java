5297b0af92e74da4a5ea6edcf2d8973acba1276c
Dragos Tanta <dragos.tanta@gmail.com>
2022-10-31 22:05:54 +0200

    curs16

---
+++ src/curs16/StringExample.java
@@ -0,0 +1,37 @@
+package curs16;
+
+public class StringExample {
+	
+	
+	public static void main(String[] args) {
+		
+		transformToString();
+	}
+	
+	
+	public static void transformToString() {
+		
+		char ch = 'a';
+		String chStr = Character.toString(ch);
+		String chStr2 = String.valueOf(ch);
+		
+		System.out.println(chStr.getClass());
+		System.out.println(chStr2.getClass());
+		
+		System.out.println("-----------------------------");
+		
+		boolean bol = true;
+		String bolStr = Boolean.toString(bol);
+		String bolStr2 = String.valueOf(bol);
+		
+		System.out.println(bolStr.getClass());
+		System.out.println(bolStr2.getClass());
+		
+		String str = null;
+		//System.out.println(str.toString());
+		System.out.println(String.valueOf(str));
+	}
+	
+	
+
+}
